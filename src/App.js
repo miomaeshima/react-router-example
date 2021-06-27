5e96c42e2b9531117277c5f8a46a21d079e1de3d
miomaeshima <miomamomo@gmail.com>
2021-06-27 13:25:06 +0900

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div>
+                <h1>Hello React Router</h1>
+            </div>
+            <Switch>
+            <Route exact path="/">
+                <Home />
+            </Route>
+            <Route path="/about">
+                <About />
+            </Route>
+            <Route path="/contact">
+                <Contact />
+            </Route>
+            <Route>
+                <NotFound />
+            </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+
+function Contact() {
+    return <h2>Contact</h2>;
+}
+
+function NotFound() {
+    return <h2>Not Found Page</h2>;
+}
 export default App;
